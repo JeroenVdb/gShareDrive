62b02ecb90a45c02a8c90e650ee0e20826dad19f
Jeroen Van den Berghe <vandenberghe.jeroen@gmail.com>
2014-03-25 09:01:28 +0100

    Still working but better folder structure

--- _site/app/js/app.js
+++ _site/app/js/app.js
@@ -10,7 +10,8 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/files-list', {templateUrl: 'partials/files-list.html', controller: 'fileListController'});
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/files-list'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,110 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+// angular.module('myApp.controllers', []).
+// 	controller('mainController', [function($scope) {
+// 		console.log("jeroen");
+// 		$scope.name = "jeroen";
+// 	}])
+// 	.controller('fileListController', [function() {
 
-  }])
-  .controller('MyCtrl2', [function() {
+// 	}])
+// 	.controller('MyCtrl1', [function() {
 
-  }]);
+// 	}])
+// 	.controller('MyCtrl2', [function() {
+
+// 	}]);
+
+var myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', function ($scope, $http, $interval) {
+
+	
+
+		$scope.name = "jeroen";
+			
+
+});
+
+myApp.controller('fileListController', function ($scope, $http, $interval) {
+
+	$scope.name = "jeroen";
+
+});
+
+var gComm = (function () {
+
+	var checkAuth = function() {
+
+		gapi.auth.authorize(
+			{'client_id': CLIENT_ID, 'scope': SCOPES.join(' '), 'immediate': true},
+			handleAuthResult
+		);
+
+	}
+
+	function handleAuthResult(authResult) {
+
+		if (authResult) {
+			// Access token has been successfully retrieved, requests can be sent to the API
+			gapi.client.load('drive', 'v2', getShareFolder);
+		} else {
+			// No access token could be retrieved, force the authorization flow.
+			gapi.auth.authorize(
+				{'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': false},
+				handleAuthResult
+			);
+		}
+	}
+
+	function getShareFolder() {
+
+		var request = gapi.client.drive.files.list({
+			'q': "title = 'Share'"
+		});
+
+		request.execute(function(resp) {
+			var folderId = resp.items[0].id;
+
+			getFilesFromSharedFolder(folderId);
+		});
+	}
+
+	function getFilesFromSharedFolder(folderId) {
+
+		var request = gapi.client.drive.children.list({
+			'folderId' : folderId
+		}); 
+
+		request.execute(function(resp) {
+			for(var i = 0; i < resp.items.length; i++) {
+				var fileId = resp.items[i].id;
+				getFileData(fileId)
+			}
+		});
+	}
+
+	function getFileData(fileId) {
+		
+		var request = gapi.client.drive.files.get({
+			'fileId': fileId
+		});
+
+		request.execute(function(resp) {
+			console.log('Title: ' + resp.thumbnailLink);
+
+			if (resp.hasOwnProperty('thumbnailLink')) {
+				var img = new Image();  ///params are optional 
+				img.src = resp.thumbnailLink;
+				document.body.appendChild(img);
+			}
+
+		});
+	}
+
+	return {
+		checkAuth:checkAuth
+	};
+
+})();
